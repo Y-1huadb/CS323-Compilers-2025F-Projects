71de8b94ec1f85ee59f250ca65255800c81919fc
Minusigma <535273272@qq.com>
2025-11-17 18:30:11 +0800

    ext

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import framework.AbstractGrader;
 import framework.project3.Grader;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,7 +7,7 @@ import java.io.InputStream;
 public class Main {
     public static void main(String[] args) throws IOException {
         {
-            InputStream input = new FileInputStream("testcases/project3/ok_01.splc");
+            InputStream input = new FileInputStream("testcases/project3/err_01.splc");
             AbstractGrader grader = new Grader(input, System.out);
             grader.run();
         }
